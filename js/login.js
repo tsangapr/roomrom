fe80f5b8c20607eeb36f651ddc6cd6446ac0d0c5
Jieun yu <jieunmaryyu@gmail.com>
2020-03-12 15:54:01 -0700

    signup and firebase added

--- js/login.js
+++ js/login.js
@@ -7,3 +7,19 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBQqckTfl3eR6oXD6TrgWie9WJVIXrrQBk",
+  authDomain: "roomroom-60e67.firebaseapp.com",
+  databaseURL: "https://roomroom-60e67.firebaseio.com",
+  projectId: "roomroom-60e67",
+  storageBucket: "roomroom-60e67.appspot.com",
+  messagingSenderId: "910945136388",
+  appId: "1:910945136388:web:dc824cfe4971426ee66a51",
+  measurementId: "G-P8XL2GE708"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+//firebase.analytics();
+ const db = firebase.firestore();
